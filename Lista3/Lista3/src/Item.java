81053a3319b1986f80dbf1f95a43afee00a9f21a
Leonardo Leite Meira <leonardo.leitemeira10@gmail.com>
2019-09-01 22:03:50 -0300

    Atualização lista3

---
+++ Lista3/Lista3/src/Item.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Item {
+    private int chave;
+    /*
+    *Outros dados
+    */
+    
+    public Item(int chave){
+        this.chave = chave;
+    }
+
+    public int getChave() {
+        return chave;
+    }
+
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+    
+    
+}
